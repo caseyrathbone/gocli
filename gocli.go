0fa812db13d3dd7c8d78f66f9610a1f8656cde9b
Casey Rathbone <casey.rathbone@gmail.com>
2017-03-27 20:31:30 -0700

    Rename and variadic update

--- gocli.go
+++ gocli.go
@@ -5,50 +5,27 @@ import (
   "errors"
 )
 
-// Provides functionality for a developer to provide a simple map of keywords
-// and functions to be executed from CLI args.
-/*
-package main
+// Intended to provide a simple mapping of keywords
+// to functions from CLI args.
 
-func main() {
-  c := cli(make[]{
-    command: "test",
-    function: &func,
-  },
-  {
-    help: "This is the default message"
-  },
-  )
-
-  _, err = c.Run();
-  if (err != nil) {
-    log.Fatal(err)
-  }
-
-  os.exit(0)
-}
-*/
-
-//type cmdFunc func(args ...interface{})
-//type defaultFunc func(unknown string)
-
-type SimpleFunc func(args ...interface{})
+type KeywordFunc func(args ...string)
 
 type CLI struct {
-  CmdMap map[string]SimpleFunc
+  CmdMap map[string] KeywordFunc
   HelperMsg string
 }
 
-func (c *CLI) Run(args []string) (int, error) {
+func (c *CLI) Run(args ...string) (int, error) {
   if len(args) < 1 {
     fmt.Println(c.HelperMsg)
-    return 1, errors.New("Uh oh...")
+    return 1, errors.New("Invalid commandline input: Expecting additional arguments")
   }
 
+  // Argument to lookup in map.
   command := args[0]
 
   if c.CmdMap[command] != nil {
-    c.CmdMap[command]()
+    c.CmdMap[command](args[1:]...)
   } else {
     fmt.Println(c.HelperMsg)
   }
